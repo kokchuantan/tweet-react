38ccafe651fdfe55d47a8a0d6a58fb302ea99981
kokchuantan <kokchuan1996@gmail.com>
2020-05-28 21:30:36 +0800

    able to display tweet information

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,16 +5,35 @@ import tweets from 'tweets'
 
 class App extends React.Component {
   render() {
+    const allTweets = tweets.tweets;
+    console.log(allTweets);
     return (
       <div>
-        tweets
+         <Tweets className = 'container' test={allTweets}/>
       </div>
+    );
+  }
+}
+
+class Tweets extends React.Component {
+  render () {
+    let tweetList = this.props.test.map(tweet => {
+      return <div>
+        <li>
+          <p>{tweet.user.screen_name}</p>
+          <p>{tweet.text}</p>
+          <p>{tweet.created_at}</p>
+        </li>
+      </div>
+    });
+    return (
+      <div className="tweets">{tweetList}</div>
     );
   }
 }
 
 const element = document.getElementById('app');
 
-ReactDOM.render(<App />, element );//
+ReactDOM.render(<App />, element );
 
 console.log("tweet react");
